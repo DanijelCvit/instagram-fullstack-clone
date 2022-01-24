2e55229f98720c4e6deda0f1519d1b02f8c16929
MineDrum <68380160+MineDrum@users.noreply.github.com>
2022-01-24 13:25:55 +0100

    Started TheHeader

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
+    <TheHeader />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import TheHeader from "@/components/TheHeader.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    TheHeader,
   },
 };
 </script>
