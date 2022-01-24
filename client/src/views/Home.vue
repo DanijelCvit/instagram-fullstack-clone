0733f2581df66ed2d3a6e52eade9c5e4b5b4ee61
DanijelCvit <danijel.c@gmail.com>
2022-01-24 17:23:09 +0100

    removed left  padding post list

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <TheHeader />
     <div class="container">
-      <ul>
+      <ul class="ps-0">
         <li v-for="post in posts" :key="post.id">
           <post-card :post="post" :user="user" />
         </li>
