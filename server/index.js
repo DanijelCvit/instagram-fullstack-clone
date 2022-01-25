df4d55689aa6a8dd853675111d06eff9fbd96804
DanijelCvit <danijel.c@gmail.com>
2022-01-25 13:14:16 +0100

    updated server with cors support

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
 import postsRoutes from "./routes/post.js";
 
 export const app = express();
 export const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
