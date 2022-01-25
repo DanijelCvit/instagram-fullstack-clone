c96257c3363339a0ebd5a5acbb0a92cc4c96a05e
Danijel C <danijelc+hyf@gmail.com>
2022-01-25 21:16:50 +0100

    changed database connection code using promises

--- server/controllers/db_connect.js
+++ server/controllers/db_connect.js
@@ -1,49 +1,48 @@
-var mysql = require('mysql');
+// var mysql = require("mysql");
 
-var con = mysql.createConnection({
-  host: "localhost",
-  port: "9000",
-  user: "root",
-  password: "",
-  database: "theGram"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   port: "3306",
+//   user: "root",
+//   password: "",
+//   database: "theGram",
+// });
 
+// function queryDatabase(sql) {
+//   con.connect(function (err) {
+//     if (err) throw err;
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       //console.log("Result: " + JSON.stringify(result));
+//       return result;
+//     });
+//     con.end();
+//   });
+// }
 
-function queryDatabase (sql) {
-  con.connect(function(err) {
-    if (err) throw err;
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      //console.log("Result: " + JSON.stringify(result));
-      return(result);
-    });
-    con.end();
-  });
-};
+// function queryPosts() {
+//   let sql = "SELECT * FROM `post`";
+//   queryDatabase(sql);
+// }
 
-function queryPosts() {
-  let sql = "SELECT * FROM `post`";
-  queryDatabase(sql);
-};
+// function queryUserPosts(userID) {
+//   let sql = "SELECT * FROM `post` WHERE user_id = " + userID;
+//   queryDatabase(sql);
+// }
 
-function queryUserPosts(userID) {
-  let sql = "SELECT * FROM `post` WHERE user_id = " + userID;
-  queryDatabase(sql);
-};
+// function queryUsers() {
+//   let sql = "SELECT * FROM `user`";
+//   queryDatabase(sql);
+// }
 
-function queryUsers() {
-  let sql = "SELECT * FROM `user`";
-  queryDatabase(sql);
-};
+// function queryComments() {
+//   let sql = "SELECT * FROM `comment`";
+//   queryDatabase(sql);
+// }
 
-function queryComments() {
-  let sql = "SELECT * FROM `comment`";
-  queryDatabase(sql);
-};
+// function generalQuery(select, from, where, is) {
+//   let sql = `SELECT ${select} FROM ${from} WHERE ${where} IS ${is}`;
+//   queryDatabase(sql);
+// }
 
-function generalQuery(select, from, where, is) {
-  let sql = `SELECT ${select} FROM ${from} WHERE ${where} IS ${is}`;
-  queryDatabase(sql);
-};
-
-queryUserPosts(10000);
+// queryUserPosts(10000);
--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,17 +1,41 @@
-let posts = [
-  {
-    postID: 1,
-    userID: 1,
-    image: "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg",
-    description: "Moroccan tajin with tafernout bread",
-    likes: 10000,
-  },
-];
+import mysql from "mysql";
+import util from "util";
+// Dont forget to do npm install to install new packages
 
+// PUT this in an .env file in server folder, then change the values to suite your database connection
+// put it into .ignore file so it's not put on github. This way we both have our own .env with our specific connections
+// # .env file
+// #
+// # Add environment-specific variables on new lines in the form of NAME=VALUE
+// #
+// DB_HOST=127.0.0.1
+// DB_USER=root
+// DB_PASS=
+// DB_PORT=3306
+// DB_NAME=thegram
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+/* The problem with previous code is that we can't return values from a callback function, since it's embedded
+  in another function. The parent function will get the result, not our function (that is outside the parent). So what we can do is
+  wrap the connection.query in a promise and use async await. This is what the below line does.
+  So this way we can just call the execQuery function with our query string and it will return the result
+  (after the Promiser resolves, so don't forget to use await).
+ */
+const execQuery = util.promisify(connection.query.bind(connection));
+
+// You can try modifying the below function to return a matching post from the database
+// You can use the code from the getPosts function and just change the mysql query
 export const getPost = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const post = posts.find((post) => post.postID === id);
+  // We need to change the below code to database query
+  // const post = post.find((post) => post.post_id === id);
 
   if (!post) {
     return res.status(404).json({ msg: `Error: no post with id: ${id}` });
@@ -20,10 +44,20 @@ export const getPost = (req, res) => {
   res.status(200).json(post);
 };
 
-export const getPosts = (req, res) => {
+// This function now returns all the posts in the database
+export const getPosts = async (req, res) => {
+  let posts;
+  try {
+    posts = await execQuery("SELECT * FROM post");
+  } catch (error) {
+    res.status(500).json({ msg: "Query to database resulted in an error" });
+  }
+
   res.status(200).json(posts);
 };
 
+// If you feel adventurous you can try also implementing the below functions, using code from getPost function
+// You can test them using Postman since we don't have any frontend forms yet
 export const createPost = (req, res) => {};
 
 export const updatePost = (req, res) => {};
