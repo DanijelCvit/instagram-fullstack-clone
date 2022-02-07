b8193748d08a695e2e2867e20ba5198bb5895604
DanijelCvit <danijel.c@gmail.com>
2022-02-07 19:37:48 +0100

    new database dump

--- database/theGram.sql
+++ database/theGram.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 25, 2022 at 03:50 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.4.18
+-- Host: 127.0.0.1
+-- Generation Time: Feb 07, 2022 at 07:33 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,57 +18,65 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `theGram`
+-- Database: `thegram`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comment`
+-- Table structure for table `comments`
 --
 
-CREATE TABLE `comment` (
-  `comment_id` int(11) NOT NULL,
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   `comment` varchar(240) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `comment`
+-- Dumping data for table `comments`
 --
 
-INSERT INTO `comment` (`comment_id`, `user_id`, `post_id`, `comment`) VALUES
+INSERT INTO `comments` (`id`, `user_id`, `post_id`, `comment`) VALUES
 (10000, 10001, 10000, 'Wow, looks delicious!');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Table structure for table `posts`
 --
 
-CREATE TABLE `post` (
-  `post_id` int(11) NOT NULL,
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `image` varchar(1000) NOT NULL,
   `description` varchar(240) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `post`
+-- Dumping data for table `posts`
 --
 
-INSERT INTO `post` (`post_id`, `user_id`, `image`, `description`) VALUES
-(10000, 10000, 'https://unsplash.com/photos/q11NM0cFFzY', 'Moroccan tajin with tafernout bread');
+INSERT INTO `posts` (`id`, `user_id`, `image`, `description`) VALUES
+(10000, 10000, 'https://images.unsplash.com/photo-1643019237176-8ae0859f1123', 'Moroccan tajin with tafernout bread'),
+(10001, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10002, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10003, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10004, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10005, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10006, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10007, 10000, 'https://images.pexels.com/photos/1643457/pexels-photo-1643457.jpeg', 'Cute cat image'),
+(10009, 10000, 'public/images/uploaded_file-1644251312179-287895624pexels-evg-kowalievska-1170986.jpg', '\"Hello Kitty\"');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `email` varchar(1000) NOT NULL,
   `first_name` varchar(1000) NOT NULL,
   `last_name` varchar(1000) NOT NULL,
@@ -78,10 +86,10 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `user` (`user_id`, `email`, `first_name`, `last_name`, `username`, `password`, `image`) VALUES
+INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `username`, `password`, `image`) VALUES
 (10000, 'brad.drummer34@yahoo.com', 'Brad', 'Haupt', 'bradhaupt', 'password', 'https://unsplash.com/photos/q11NM0cFFzY'),
 (10001, 'tester@testnet.com', 'Elvis', 'Presley', 'tester', 'password', 'https://unsplash.com/photos/QeVmJxZOv3k');
 
@@ -90,47 +98,64 @@ INSERT INTO `user` (`user_id`, `email`, `first_name`, `last_name`, `username`, `
 --
 
 --
--- Indexes for table `comment`
+-- Indexes for table `comments`
 --
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`comment_id`),
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `post_id` (`post_id`);
 
 --
--- Indexes for table `post`
+-- Indexes for table `posts`
 --
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`post_id`),
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `users`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `comment`
+-- AUTO_INCREMENT for table `comments`
 --
-ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
 
 --
--- AUTO_INCREMENT for table `post`
+-- AUTO_INCREMENT for table `posts`
 --
-ALTER TABLE `post`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10001;
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10010;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10002;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10002;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `FK_comment_post` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `FK_comment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `FK_post_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
