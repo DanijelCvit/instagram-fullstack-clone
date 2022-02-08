7e6fde5ec9c555ddd4946fdee8754f4871c22c16
MineDrum <contact.bradhaupt@gmail.com>
2022-02-08 09:56:01 +0100

    Made user.js files

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -10,8 +10,6 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-console.log(process.env);
-
 // Convert query function with callback into a Promise
 const execQuery = util.promisify(connection.query.bind(connection));
 
@@ -48,15 +46,3 @@ export const updatePost = async (req, res) => {
 export const deletePost = async (req, res) => {
 
 };
-
-export const getUser = async (req, res) => {
-  let user;
-  try {
-    user = await execQuery(`SELECT * FROM user WHERE id = 10000`); 
-  } catch (error) {
-    res.status(500).json({ msg: "User query to database resulted in an error" });
-  }
-
-  res.status(200).json(user);
-};
-
