593f985de9dcd996fbdbd7d1070122411dd30845
DanijelCvit <danijel.c@gmail.com>
2022-02-15 16:34:09 +0100

    Removed commented out code

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,6 @@ import upload from "../storage.js";
 
 const router = express.Router();
 
-//router.get("/", getUser)
 router.get("/:id", getUser);
 router.post("/", upload.single("uploaded_file"), createUser);
 router.patch("/:id", upload.single("uploaded_file"), updateUser);
