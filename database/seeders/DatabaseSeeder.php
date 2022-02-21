50303dd8078af20883cc7bd1e4ff9a423c4c78c9
Danijel Cvit <danijel.c+github@gmail.com>
2022-02-21 14:29:35 +0100

    Seeds comments as well now

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Post::factory(5)->create();
+
+        Comment::factory(3)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
