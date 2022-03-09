313cfb22d70a5fbc05dda8163471cceef34174fd
Brad Haupt <contact@bradhaupt.com>
2022-03-09 15:49:23 +0100

    Added Search Bar Functionality
    Can search by username, first name, and last name

--- resources/js/components/TheSearch.vue
+++ resources/js/components/TheSearch.vue
@@ -8,9 +8,11 @@
     <div class="position-relative">
       <input
         @click="resultsVisible = !resultsVisible"
-        type="text"
+				@keyup="search"
+        v-model="searchQuery"
+				type="text"
         class="form-control d-block"
-        name=""
+				name=""
         placeholder="Search"
       />
       <div v-if="resultsVisible" class="search-popup position-absolute">
@@ -19,10 +21,23 @@
         </div>
         <div class="search-content shadow-sm p-3 mb-5 bg-body rounded">
           <h2 class="search-header">Search</h2>
-          <div
-            class="search-body d-flex justify-content-center align-items-center"
-          >
-            <p>No recent searches.</p>
+          <div class="search-body justify-content-center align-items-center">
+            <p v-if="results.length===0">No recent searches.</p>
+						<div v-if="results.length!==0" v-for="result in results" class="d-block">
+							<a :href="'/#/user/'+result.username" class="text-decoration-none link-dark">
+								<div class="my-2 row">
+									<div class="col-3">
+										<img :src="result.image" :alt="result.username" class="rounded-circle profile-pic">
+									</div>
+									<div class="col-9">
+										<span class="h6">@{{result.username}}</span>
+										<br>
+										<span>{{result.first_name}}</span>
+										<span>{{result.last_name}}</span>
+									</div>
+								</div>
+							</a>
+						</div>
           </div>
         </div>
       </div>
@@ -35,8 +50,25 @@ export default {
   data() {
     return {
       resultsVisible: false,
+			searchQuery: '',
+			results: [],
     };
   },
+	methods: {
+		async search(e){
+
+			try {
+				const res = await fetch(`/api/user/search/${this.searchQuery}`)
+				const results = await res.json()
+				this.results = await results
+
+			} catch {
+
+			} finally {
+
+			}
+		}
+	}
 };
 </script>
 
@@ -61,7 +93,7 @@ export default {
 }
 .search-body {
   width: 250px;
-  height: 500px;
+
   font-size: 0.7rem;
   font-weight: normal;
 }
@@ -72,5 +104,11 @@ export default {
   left: 0;
   width: 100vw;
   height: 100vh;
+}
+.profile-pic {
+	width: 100%;
+	height: auto;
+	aspect-ratio: 1/1;
+	object-fit: cover;
 }
 </style>
